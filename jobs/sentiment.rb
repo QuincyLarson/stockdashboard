5ba05b149294f0c78e60656b688d57bfa7ba9b73
Joe Cabrera <calcmaster16@gmail.com>
2014-06-04 01:01:13 +0000

    checks for sentiment

--- jobs/sentiment.rb
+++ jobs/sentiment.rb
@@ -2,14 +2,16 @@ SCHEDULER.every '30m', :first_in => 0 do |job|
   begin
     require 'cgi'
     sentiment = JSON.parse((URI.parse("http://ec2-54-83-155-47.compute-1.amazonaws.com:5000/api/v1/sentiment?symbol=twtr").read)).first[1]
+    color = "#C0C0C0"
     if sentiment
       sentiment = sentiment.map do |sentiment|
         if sentiment == "positive"
-        if sentiment == "neutral"
+            color = "#00FF00"
         if sentiment == "negative"
+            color = "#FF0000"
       end
     end
-    send_event('twitter_sentiment', comments: tweets)
+    send_event('twitter_sentiment', color: color)
   rescue
   end
 end
